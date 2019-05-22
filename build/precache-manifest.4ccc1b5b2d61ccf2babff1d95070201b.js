09288bbfd23b4050da5dac3fd491731fd3364d5e
Andreas Soelberg <ansoe@dfds.com>
2019-05-22 13:53:25 +0200

    added build folder to git

---
+++ build/precache-manifest.4ccc1b5b2d61ccf2babff1d95070201b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0e25da08adef387a92c02a2cad0b531",
+    "url": "/soelberg484/Wifi-portal/static/media/wifi-svg.b0e25da0.svg"
+  },
+  {
+    "revision": "bbd439b2da1a2fc12b4dc302d310c3c7",
+    "url": "/soelberg484/Wifi-portal/static/media/logo.bbd439b2.svg"
+  },
+  {
+    "revision": "6062f80d5d3bf040d8ed2b8e5af5a77d",
+    "url": "/soelberg484/Wifi-portal/static/media/action-blue.6062f80d.svg"
+  },
+  {
+    "revision": "a959a802d92c99d6d4256ae9cbf29657",
+    "url": "/soelberg484/Wifi-portal/static/media/Background_Image.a959a802.png"
+  },
+  {
+    "revision": "dc9183990fdbaf61b5eb",
+    "url": "/soelberg484/Wifi-portal/static/js/runtime~main.fea53607.js"
+  },
+  {
+    "revision": "8b9afea49d879b0a2f79",
+    "url": "/soelberg484/Wifi-portal/static/js/main.e4cbdc51.chunk.js"
+  },
+  {
+    "revision": "d9a80b3bfbd5885b4c25",
+    "url": "/soelberg484/Wifi-portal/static/js/2.2d429cc2.chunk.js"
+  },
+  {
+    "revision": "8b9afea49d879b0a2f79",
+    "url": "/soelberg484/Wifi-portal/static/css/main.88f279a5.chunk.css"
+  },
+  {
+    "revision": "adae83a24fc70cf51da9273520bdb196",
+    "url": "/soelberg484/Wifi-portal/index.html"
+  }
+];
